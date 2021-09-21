f43ada33899545c53aa8745276fc0a32c5abee41
Yuri Hrynkiv <yuhrynkiv@gmail.com>
2021-09-21 11:59:53 +0300

    major: added config service and env files supporting

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors();
+
+  const configService = app.get<ConfigService>(ConfigService);
+
+  const port = configService.get<number>('WILSON_BE_PORT', 8080);
+  await app.listen(port);
 }
 bootstrap();
