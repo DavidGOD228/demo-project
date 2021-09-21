ef81bd4b0f250ed1da942612c365fd99a3b3592e
Yuri Hrynkiv <yuhrynkiv@gmail.com>
2021-09-21 13:28:18 +0300

    minor: added swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -8,6 +9,15 @@ async function bootstrap() {
   app.enableCors();
 
   const configService = app.get<ConfigService>(ConfigService);
+
+  const options = new DocumentBuilder()
+    .setTitle('WILSON')
+    .setDescription('Backend Application')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
 
   const port = configService.get<number>('WILSON_BE_PORT', 8080);
   await app.listen(port);
