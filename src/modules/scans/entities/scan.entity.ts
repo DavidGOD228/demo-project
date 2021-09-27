093438ab167df7f94edfe29e9f8d4605ddea47a9
Yuri Hrynkiv <yuhrynkiv@gmail.com>
2021-09-27 17:49:07 +0300

    minor: WIL-16: Connect AWS Rekognition to the backend

--- src/modules/scans/entities/scan.entity.ts
+++ src/modules/scans/entities/scan.entity.ts
@@ -1,5 +1,7 @@
 import { Channel } from 'src/modules/channels/entities/channel.entity';
 import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from '../../users/entities/user.entity';
+import { Widget } from '../../widgets/entities/widget.entity';
 
 @Entity({ schema: 'cmn', name: 'scans' })
 export class Scan {
@@ -10,9 +12,17 @@ export class Scan {
   number: number;
 
   @Column()
-  objectId: number;
+  objectId: string;
 
   @ManyToOne(() => Channel, channel => channel.scans)
   @JoinColumn({ name: 'channel_id' })
   channel: Channel;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'object_id' })
+  user: User;
+
+  @ManyToOne(() => Widget)
+  @JoinColumn({ name: 'object_id' })
+  widget: Widget;
 }
