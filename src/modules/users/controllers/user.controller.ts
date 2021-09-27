66427d76dc64d0cab0f5bcdc4125b7092d2b2e6d
Kovalova22 <ikovalova333@gmail.com>
2021-09-27 12:56:41 +0300

    minor: WILL-34: add change onboarded status

--- src/modules/users/controllers/user.controller.ts
+++ src/modules/users/controllers/user.controller.ts
@@ -1,9 +1,9 @@
 import { Body, Controller, Patch, Req, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiNotFoundResponse, ApiOkResponse, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
-import { RequestWithUserParams } from 'src/common/interfaces';
+import { RequestWithUserParams, SuccessResponseMessage } from 'src/common/interfaces';
 import { JwtAuthGuard } from 'src/modules/auth/guards/jwt-auth.guard';
 import { User } from '../entities/user.entity';
-import { updateProfileDto } from '../interfaces/user.dto';
+import { ChangeUserOnBoardedStatusDto, UpdateProfileDto } from '../interfaces/user.dto';
 import { UserService } from '../services/user.service';
 
 @UseGuards(JwtAuthGuard)
@@ -17,9 +17,26 @@ export class UserController {
   @ApiOkResponse({ description: 'OK' })
   @ApiUnauthorizedResponse({ description: 'Unauthorized' })
   @Patch('profile')
-  async updateProfile(@Body() body: updateProfileDto, @Req() req: RequestWithUserParams): Promise<User> {
+  async updateProfile(@Body() body: UpdateProfileDto, @Req() req: RequestWithUserParams): Promise<User> {
     try {
       return await this.usersService.updateProfile(body, req.user.id);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  @ApiTags('Users')
+  @ApiBearerAuth()
+  @ApiOkResponse({ description: 'OK' })
+  @ApiNotFoundResponse({ description: 'Not Found' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @Patch('onboard')
+  async changeUserOnBoardedStatus(
+    @Body() body: ChangeUserOnBoardedStatusDto,
+    @Req() req: RequestWithUserParams,
+  ): Promise<SuccessResponseMessage> {
+    try {
+      return await this.usersService.changeUserOnBoardedStatus(body, req.user.id);
     } catch (error) {
       console.log(error.message);
     }
--- src/modules/users/services/user.service.ts
+++ src/modules/users/services/user.service.ts
@@ -1,14 +1,15 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { SuccessResponseMessage } from 'src/common/interfaces';
 import { Repository } from 'typeorm';
 import { User } from '../entities/user.entity';
-import { updateProfileDto } from '../interfaces/user.dto';
+import { ChangeUserOnBoardedStatusDto, UpdateProfileDto } from '../interfaces/user.dto';
 
 @Injectable()
 export class UserService {
   constructor(@InjectRepository(User) private readonly usersRepository: Repository<User>) {}
 
-  public async updateProfile(body: updateProfileDto, userId: string): Promise<User> {
+  public async updateProfile(body: UpdateProfileDto, userId: string): Promise<User> {
     const user = await this.usersRepository.findOne(userId);
 
     if (!user) {
@@ -18,5 +19,17 @@ export class UserService {
     const updatedUser = { ...user, ...body };
     await this.usersRepository.update(user.id, updatedUser);
     return updatedUser;
+  }
+
+  public async changeUserOnBoardedStatus(
+    body: ChangeUserOnBoardedStatusDto,
+    userId: string,
+  ): Promise<SuccessResponseMessage> {
+    const user = await this.usersRepository.findOne(userId);
+    if (!user) {
+      throw new NotFoundException();
+    }
+    await this.usersRepository.update(user.id, { onboarded: body.onboarded });
+    return { message: 'User on-boarded status successfully updated!' };
   }
 }
