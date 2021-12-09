95405780ea31f5f95ea941673c9f51bc4f299d05
Yuri Hrynkiv <yuhrynkiv@gmail.com>
2021-12-09 16:42:34 +0200

    patch: updated logic of checking if user has already scanned certain widget

--- src/modules/aws/interfaces/getWidgetPromotion.dto.ts
+++ src/modules/aws/interfaces/getWidgetPromotion.dto.ts
@@ -1,6 +1,8 @@
 import { IsOptional, IsString } from 'class-validator';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 
 export class GetWidgetPromotionDto {
+  @ApiPropertyOptional()
   @IsOptional()
   @IsString()
   widgetId?: string;
