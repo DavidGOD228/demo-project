0ab9aba92d235fa24a0c6ed6c65ebed6f8e9b830
Kovalova22 <ikovalova333@gmail.com>
2025-03-17 17:29:11 +0200

    update accuracy

--- src/modules/aws/interfaces/accuracy.enum.ts
+++ src/modules/aws/interfaces/accuracy.enum.ts
@@ -1,20 +1,20 @@
 import { InscriptionLabelEnum, LeagueLabelEnum, TypeLabelEnum } from '../../channels/interfaces/channel.enum';
 
 export const TypeLabelEnumAccuracyEnum = {
-  [TypeLabelEnum.BASKETBALLBALL]: 80,
-  [TypeLabelEnum.FOOTBALLBALL]: 80,
+  [TypeLabelEnum.BASKETBALLBALL]: 50,
+  [TypeLabelEnum.FOOTBALLBALL]: 50,
 };
 
 export const LeagueLabelAccuracyEnum = {
-  [LeagueLabelEnum.NBALOGO]: 80,
-  [LeagueLabelEnum.NBA75]: 80,
-  [LeagueLabelEnum.WNBALOGO]: 80,
-  [LeagueLabelEnum.EVOLUTION]: 80,
-  [LeagueLabelEnum.EVONXT]: 80,
-  [LeagueLabelEnum.NFLSHIELD]: 80,
-  [LeagueLabelEnum.NFLLETTERING]: 80,
+  [LeagueLabelEnum.NBALOGO]: 50,
+  [LeagueLabelEnum.NBA75]: 50,
+  [LeagueLabelEnum.WNBALOGO]: 50,
+  [LeagueLabelEnum.EVOLUTION]: 50,
+  [LeagueLabelEnum.EVONXT]: 50,
+  [LeagueLabelEnum.NFLSHIELD]: 50,
+  [LeagueLabelEnum.NFLLETTERING]: 50,
 };
 
 export const InscriptionLabelAccuracyEnum = {
-  [InscriptionLabelEnum.WILSONSCRIPT]: 80,
+  [InscriptionLabelEnum.WILSONSCRIPT]: 50,
 };
